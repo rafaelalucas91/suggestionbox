1f0d5c1f9373cdf979ed15885557f50da50654ba
rafaelalucas91 <rafaelalucas_91@hotmail.com>
2019-11-19 22:24:39 +0000

    upd: css

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 window.addEventListener("load", event => {
+    var btnSend = document.querySelector('.send'),
+        paper = document.querySelector('.paper');
 
+    btnSend.addEventListener('click', foldPaper);
+
+    function foldPaper() {
+        paper.classList.add('fold');
+        btnSend.style.display = "none";
+
+        setTimeout(function () {
+            paper.classList.remove('fold');
+            btnSend.style.display = "block";
+        }, 2000);
+    }
 
 });
