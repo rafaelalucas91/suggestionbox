fb372d88f28eac34f44ff792b8555e552cea21fd
rafaelalucas91 <rafaelalucas_91@hotmail.com>
2019-11-19 23:59:33 +0000

    upd: css + form

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 window.addEventListener("load", event => {
     var btnSend = document.querySelector('.send'),
-        paper = document.querySelector('.paper');
+        paper = document.querySelector('.paper'),
+        form = document.querySelector('.paper__form');
 
     btnSend.addEventListener('click', foldPaper);
 
     function foldPaper() {
         paper.classList.add('fold');
+        form.classList.add('fade');
         btnSend.style.display = "none";
 
         setTimeout(function () {
             paper.classList.remove('fold');
             btnSend.style.display = "block";
+            form.classList.remove('fade');
         }, 2000);
     }
 
